57d04a30ebd3ef48081cf30972c21fdb6fc1da70
ricardovaleri0 <ricardovaleriosilvaoliveira@gmail.com>
2025-09-13 19:07:51 -0300

    new files

--- .vscode/modificadoresDados.c
+++ .vscode/modificadoresDados.c
@@ -45,12 +45,33 @@
         printf("Tamanho de double: %zu bytes\n", sizeof(double));
         printf("Tamanho de long double: %zu bytes\n", sizeof(long double));
 
-        double numeroPreciso = 3.14159265358979323846; // double padrão
-        long double numeroMuitoPreciso = 3.141592653589793238463L; // long double para maior precisão
+        double numeroPreciso = 3.141592653589793; // double padrão
+        double numeroMuitoPreciso = 3.141592653589793238463; // long double para maior precisão porém para microsoft long double ou double é  mesma coisa
 
         printf("Número preciso (double): %.15f\n", numeroPreciso);
-        printf("Número muito preciso (long double): %.21Lf\n", numeroMuitoPreciso);
-        
+        printf("Número muito preciso (long double): %.21lf\n", numeroMuitoPreciso);// %lf para long double em alguns compiladores
+
+        unsigned long int numeroGrandePositivo = 4000000000; // Correto, pois unsigned long int
+        unsigned int numeroPositivo = 4000000000; // Correto, pois unsigned int
+        long long int numeroGrande2 = 4000000000; // Incorreto, ultrapassa o limite de long int
+        int numero = 4000000000; // Incorreto, ultrapassa o limite de int
+
+        printf("Número grande positivo (unsigned long int): %lu\n", numeroGrandePositivo);
+        printf("Número positivo (unsigned int): %u\n", numeroPositivo); 
+        printf("Número grande (long long int): %lld\n", numeroGrande2);
+        printf("Número (int): %d\n", numero);
+
+        short int numeroPequeno = 32767; // Correto, dentro do limite de short int
+        printf("Número pequeno (short int): %hd\n", numeroPequeno);
+
+        numeroPequeno = 32768; // Incorreto, ultrapassa o limite de short int
+        printf("Número pequeno atualizado (short int): %d\n", numeroPequeno);
+
+        printf("\n*** Tamanho das variáveis ***\n");
+        printf("Short int: %lu B - int: %lu B - Long int: %lu B - Long long int: %lu B\n", sizeof(short int), sizeof(int), sizeof(long int), sizeof(long long int));
+        printf("Float: %lu B - Double: %lu B - Long double: %lu B\n", sizeof(float), sizeof(double), sizeof(long double));
+
+
 
         return 0;
     }
