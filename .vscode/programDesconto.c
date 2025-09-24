c59e90ad5862295a20ec8857af22f70a6970442c
ricardovaleri0 <ricardovaleriosilvaoliveira@gmail.com>
2025-09-23 22:35:52 -0300

    Criação de Estrutura aninhada e encadeada if

--- .vscode/programDesconto.c
+++ .vscode/programDesconto.c
@@ -55,5 +55,34 @@ if (idad >= 18 && idad < 65) {
     printf("Você não está qualificado pela idade.\n");
 }
 
+int numero;
+printf("Digite um número: \n");
+scanf("%d", &numero);
+
+if (numero > 0) {
+    if(numero % 2 == 0) {
+        printf("O número é positivo e par.\n");
+    } else {
+        printf("O número é positivo e ímpar.\n");
+    }
+} else if (numero == 0) {
+    printf("O número é zero.\n");
+} else {
+    printf("O número é negativo.\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 return 0;
 }
