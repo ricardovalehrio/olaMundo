6a0ef118956d4b33d85bbfff1264b7c54bc3608b
ricardovaleri0 <ricardovaleriosilvaoliveira@gmail.com>
2025-09-29 20:34:58 -0300

    Jogo Jokenpo

---
+++ .vscode/jokenpo.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+    int escolhaJogador, escolhaComputador;
+    srand(time(0)); // Inicializa o gerador de números aleatórios
+
+    printf("*** Bem-vindo ao Jokenpo! ***\n");
+    printf("Escolha uma opção:\n");
+    printf("1 - Pedra\n");
+    printf("2 - Papel\n");
+    printf("3 - Tesoura\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &escolhaJogador);
+
+    escolhaComputador = rand() % 3 + 1; // Gera uma escolha aleatória para o computador
+
+    switch (escolhaJogador)
+    {
+    case 1: // Pedra
+        printf("Você escolheu Pedra.\n");
+        break;
+
+    case 2: // Papel
+        printf("Você escolheu Papel.\n");
+        break;
+
+    case 3: // Tesoura
+        printf("Você escolheu Tesoura.\n");
+        break;
+
+    default:
+        printf("Escolha inválida! Por favor, escolha 1, 2 ou 3.\n");
+        break;
+    }
+
+    printf("A escolha do computador foi: ");
+    switch (escolhaComputador)
+    {
+    case 1:
+        printf("Pedra.\n");
+        break;
+    case 2:
+        printf("Papel.\n");
+        break;
+    case 3:
+        printf("Tesoura.\n");
+        break;
+    }
+
+    // Determina o vencedor
+    if (escolhaJogador == escolhaComputador)
+    {
+        printf("### Empate! ###\n");
+    }
+    else if ((escolhaJogador == 1 && escolhaComputador == 3) ||
+             (escolhaJogador == 2 && escolhaComputador == 1) ||
+             (escolhaJogador == 3 && escolhaComputador == 2))
+    {
+        printf("*** Você venceu! ***\n");
+    }
+    else
+    {
+        printf("xxx Você perdeu! xxx\n");
+    }
+
+    return 0;
+}
